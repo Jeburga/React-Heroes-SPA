dfea067e078d956391af055ca67709e1451617e2
Jesus <jeburga9708@gmail.com>
2024-12-26 16:29:46 -0500

    Cards configuradas

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -20,48 +20,16 @@ export const HeroCard = ({
 
     const heroImageUrl = `/assets/heroes/${ id }.jpg`;
 
-    // const charactesByHero =  (<p>{ characters }</p>);
-
-
     return (
         <div className="col animate__animated animate__fadeIn">
-            <div className="card">
-
-                <div className="row no-gutters">
-                    
-                    <div className="col-4">
-                        <img src={ heroImageUrl } className="card-img" alt={ superhero } />
-                    </div>
-
-                    <div className="col-8">
-
-                        <div className="card-body">
-
-                            <h5 className="card-title">{ superhero }</h5>
-                            <p className="card-text">{ alter_ego }</p>
-
-                            {/* {
-                                ( alter_ego !== characters ) && charactesByHero
-                                ( alter_ego !== characters ) && <p>{ characters }</p>
-                            } */}
-                            <CharactersByHero characters={ characters } alter_ego={ alter_ego } />
-
-                            <p className="card-text">
-                                <small className="text-muted">{ first_appearance }</small>
-                            </p>
-
-                            <Link to={`/hero/${ id }`}>
-                                Más..
-                            </Link>
-
-                            
-                        </div>
-
-                    </div>
-
-
+            <div className="card m-4 align-items-center text-center" style={{width: "18rem"}}>
+                <img src={ heroImageUrl } className="card-img-top" alt={ id } />
+                <div className="card-body">
+                    <h5 className="card-title">{ superhero }</h5>
+                    <p className="card-text">{ alter_ego }</p>
+                    <p className="card-text">{ first_appearance }</p>
+                    <a href="#" className="btn btn-primary">Ver más</a>
                 </div>
-
             </div>
         </div>
     )
