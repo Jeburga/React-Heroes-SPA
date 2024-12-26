a94924d08294f338dedc0174f92623571f4ad39e
Jesus <jeburga9708@gmail.com>
2024-12-26 16:45:01 -0500

    Cards mejoradas

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,13 +1,9 @@
 import { Link } from 'react-router-dom';
 
-const CharactersByHero = ({ alter_ego, characters}) => {
-    // if ( alter_ego === characters ) return (<></>);
-    // return <p>{ characters }</p>
-    return ( alter_ego === characters )
-     ? <></>
-     : <p>{ characters }</p>;
-}
-
+const CharactersByHero = ({ alter_ego, characters }) => {
+    if ( alter_ego == characters ) return (<></>);
+    return <p>{ characters }</p>
+};
 
 export const HeroCard = ({ 
     id,
@@ -27,8 +23,11 @@ export const HeroCard = ({
                 <div className="card-body">
                     <h5 className="card-title">{ superhero }</h5>
                     <p className="card-text">{ alter_ego }</p>
-                    <p className="card-text">{ first_appearance }</p>
-                    <a href="#" className="btn btn-primary">Ver más</a>
+                    <CharactersByHero characters={ characters } alter_ego={ alter_ego } />
+                    <p className="card-text">
+                        <small className="text-muted">{ first_appearance }</small>
+                    </p>
+                    <Link to={`/hero/${id}`} className="btn btn-primary">Ver más</Link>
                 </div>
             </div>
         </div>
